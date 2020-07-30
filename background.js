3f79bad15cb2386d5fd54c390fb9c5e33aa14ab3
hardik <hardik96bansal@gmail.com>
2020-07-30 17:39:54 +0530

    Modified layout

--- background.js
+++ background.js
@@ -1,22 +1,19 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.captureVisibleTab(null, function (img) {
         var a = document.createElement("img");
-        console.log(img);
         a.innerHTML = '<img src="' + img + '">';
-
-
         var link = document.createElement('a');
         link.innerHTML = 'download image';
         link.addEventListener('click', function (ev) {
             link.href = img;
-            link.download = "mypainting.jpeg";
+            link.download = "Screenshot.jpeg";
         }, false);
         document.body.appendChild(link);
         //link.click()
         //chrome.downloads.showDefaultFolder();
         chrome.downloads.download({
             url: img,
-            filename: "sss/aa",
+            filename: "folder/Screenshot",
             saveAs: false
         });
     });
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 document.getElementById("download").addEventListener("click", function () {
    var imgElem = document.getElementById("hidden").getAttribute('href');
    var folder = document.getElementById("folder").value;
+   var fName = 'Screenshot.jpeg';
+   if(folder!='' && folder!=undefined) fName = folder+'/'+fName;
    chrome.downloads.download({
       url: imgElem,
-      filename: folder + "/Screenshot.jpeg",
+      filename: fName,
       saveAs: false
    });
-
+   setInterval(()=>window.close(), 500);
 });
 
 chrome.tabs.captureVisibleTab(null, function (img) {
@@ -16,5 +18,8 @@ chrome.tabs.captureVisibleTab(null, function (img) {
    link.href = img;
    document.body.appendChild(link);
    document.getElementById("hidden").style.display = "none";
+
+   var image = document.getElementById("screenshot");
+   image.setAttribute('src',img);
 });
 
